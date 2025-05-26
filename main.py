8c5460b723575561f39ef9f8140464fae23814c7
Shoaib Raza <rshoaib376@gmail.com>
2025-05-26 09:54:16 +0500

    Add files via upload
    add fast api

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def data1():
+    return {"message":" hello my dear class I am here for you. "}
+
+@app.get("/api")
+def data2():
+    return [
+        {
+            "name":"Adil",
+            "age": 23,
+            "class":"MS Artificial Intelligence"
+        },
+           {
+            "name":"ahmad",
+            "age": 23,
+            "class":"MS Artificial Intelligence"
+        },
+           {
+            "name":"Moeez",
+            "age": 23,
+            "class":"MS Artificial Intelligence"
+        },
+    ]
